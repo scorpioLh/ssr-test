ab6e1a5e703f9a939d44ae35a997d91263359b95
linhan <scorpio360@163.com>
2019-06-30 13:39:46 +0800

    完成nuxt的学习

--- hello-nuxt/layouts/default.vue
+++ hello-nuxt/layouts/default.vue
@@ -3,6 +3,7 @@
     <nuxt-link to="/">首页</nuxt-link>
     <nuxt-link to="/home">HOME</nuxt-link>
     <nuxt-link to="/orders">Order</nuxt-link>
+    <nuxt-link to="/counter">Counter</nuxt-link>
     <nuxt-link to="/goods_list">商品列表(动态路由传参)</nuxt-link>
     <nuxt-link to="/users">用户</nuxt-link>
     <nuxt />
---
+++ hello-nuxt/pages/counter.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>{{$store.state.count}}</p>
+    <button @click="$store.commit('add')">+1</button>
+    <hr />
+    <p>{{$store.state.subcount.scount}}</p>
+    <button @click="$store.commit('subcount/addScount')">+1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    console.log(this.$store.state)
+  }
+}
+</script>
+
